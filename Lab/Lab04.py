a55cf72e3eb6df5720bd09f6494b416079b53ac0
SinceOctober <281491295@qq.com>
2025-02-19 10:43:12 +0000

    Update Lab04.py

--- Lab/Lab04.py
+++ Lab/Lab04.py
@@ -143,5 +143,11 @@ def add_chars(w1, w2):
     ...       ['For', 'While', 'Set', 'SetComp']) # Must use recursion
     True
     """
+    if not w1:
+        return w2
+    if w1[0] == w2[0]:
+        return add_chars(w1[1:], w2[1:])
+    else:
+        return w2[0] + add_chars(w1, w2[1:])
     
 
