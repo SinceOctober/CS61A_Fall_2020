75b9509c8552ef428d94bb531c6891b45de552d9
SinceOctober <281491295@qq.com>
2025-03-05 15:24:50 +0000

    Update hog.py

--- Hog/hog.py
+++ Hog/hog.py
@@ -46,7 +46,7 @@ def free_bacon(score):
 
     # Trim pi to only (score + 1) digit(s)
     # BEGIN PROBLEM 2
-    "*** YOUR CODE HERE ***"
+    pi //= 10 ** (100 - score)
     # END PROBLEM 2
 
     return pi % 10 + 3
