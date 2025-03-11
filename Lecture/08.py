25753369fdee3b1c8c957f72be261a87ded4e72a
SinceOctober <281491295@qq.com>
2025-03-11 15:43:42 +0000

    Update 08.py

--- Lecture/08.py
+++ Lecture/08.py
@@ -39,6 +39,24 @@ def sum_digits_rec(n, digit_sum):
         n, last = split(n)
         return sum_digits_rec(n, digit_sum + last)
 
+
+def luhn_sum(n):
+    """Return the Luhn sum of n."""
+    if n < 10:
+        return n
+    else:
+        all_but_last, last = split(n)
+        return luhn_sum_double(all_but_last) + last
+    
+def luhn_sum_double(n):
+    all_but_last, last = split(n)
+    luhn_digit = sum_digits(2 * last)
+    if n < 10:
+        return luhn_digit
+    else:
+        return luhn_sum(all_but_last) + luhn_digit
+
+
 # Lists
 
 odds = [41, 43, 47, 49]
