bfb846765aadb61405b81495f28502bf5d9a48e4
SinceOctober <281491295@qq.com>
2025-03-17 15:43:24 +0000

    Update hog.py

--- Hog/hog.py
+++ Hog/hog.py
@@ -118,7 +118,7 @@ def pig_pass(player_score, opponent_score):
     False
     """
     # BEGIN PROBLEM 4b
-    "*** YOUR CODE HERE ***"
+    return player_score < opponent_score and opponent_score - player_score < 3
     # END PROBLEM 4b
 
 
@@ -157,7 +157,18 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
     """
     who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
-    "*** YOUR CODE HERE ***"
+    while score0 < goal and score1 < goal:
+        if who == 0:
+            num_rolls = strategy0(score0, score1)
+            score0 += take_turn(num_rolls, score1, dice)
+            if not extra_turn(score0, score1):
+                who = other(who)
+        else:
+            num_rolls = strategy1(score1, score0)
+            score1 += take_turn(num_rolls, score0, dice)
+            if not extra_turn(score1, score0):
+                who = other(who)
+        say = say(score0, score1)
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
