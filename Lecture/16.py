ad1d4cc29c92fbeb52707f57158e5fd756f96169
SinceOctober <281491295@qq.com>
2025-04-02 12:39:12 +0100

    Updated 16.py

--- Lecture/16.py
+++ Lecture/16.py
@@ -8,6 +8,17 @@ def make_withdraw(balance):
         return balance
     return withdraw
 
+# Also works:
+def make_withdraw1(balance):
+    """Return a withdraw function with a starting balance."""
+    def withdraw(amount):
+        # nonlocal balance
+        if amount > balance:
+            return 'Insufficient funds'
+        # balance = balance - amount
+        return balance
+    return withdraw
+
 def make_withdraw_list(balance):
     b = [balance]
     def withdraw(amount):
