91602e8a7b013c2a1103c4d69567ff7ab8518c28
SinceOctober <281491295@qq.com>
2025-04-03 15:26:00 +0100

    Update 17.py

--- Lecture/17.py
+++ Lecture/17.py
@@ -55,3 +55,43 @@ def iterator_demos():
 def double(x):
     print('***', x, '=>', 2*x, '***')
     return 2*x
+
+def built_in_demo():
+    """Using built-in sequence functions.
+
+    >>> bcd = ['b', 'c', 'd']
+    >>> [x.upper() for x in bcd]
+    ['B', 'C', 'D']
+    >>> caps = map(lambda x: x.upper(), bcd)
+    >>> next(caps)
+    'B'
+    >>> next(caps)
+    'C'
+    >>> s = range(3, 7)
+    >>> doubled = map(double, s)
+    >>> next(doubled)
+    *** 3 => 6 ***
+    6
+    >>> next(doubled)
+    *** 4 => 8 ***
+    8
+    >>> list(doubled)
+    *** 5 => 10 ***
+    *** 6 => 12 ***
+    [10, 12]
+    >>> f = lambda x: x < 10
+    >>> a = filter(f, map(double, reversed(s)))
+    >>> list(a)
+    *** 6 => 12 ***
+    *** 5 => 10 ***
+    *** 4 => 8 ***
+    *** 3 => 6 ***
+    [8, 6]
+    >>> t = [1, 2, 3, 2, 1]
+    >>> reversed(t) == t
+    False
+    >>> list(reversed(t)) == t
+    True
+    >>> d = {'a': 1, 'b': 2, 'c': 3}
+    >>> items = iter(zip(d.keys(), d.values())) # Call next(items)
+    """
