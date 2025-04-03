42f860c5e5fd2affd7bcaef7cdc25034991c4379
SinceOctober <281491295@qq.com>
2025-04-03 15:31:15 +0100

    Update 17.py

--- Lecture/17.py
+++ Lecture/17.py
@@ -124,3 +124,23 @@ def evens(start, end):
     while even < end:
         yield even
         even += 2
+
+class Countdown:
+    """Count down to zero.
+
+    >>> list(Countdown(5))
+    [5, 4, 3, 2, 1]
+    >>> for x in Countdown(3):
+    ...     print(x)
+    3
+    2
+    1
+    """
+    def __init__(self, start):
+        self.start = start
+
+    def __iter__(self):
+        v = self.start
+        while v > 0:
+            yield v
+            v -= 1
