a402aade93af36de4b48ce5d529cd2535c02db44
SinceOctober <281491295@qq.com>
2025-04-09 09:58:01 +0100

    Update the hw04.py

--- Homework/hw04/hw04.py
+++ Homework/hw04/hw04.py
@@ -244,6 +244,7 @@ def remainders_generator(m):
     for i in range(m):
         yield remainder_gen(i)
 
+
 def naturals():
     """A generator function that yields the infinite sequence of natural
     numbers, starting at 1.
