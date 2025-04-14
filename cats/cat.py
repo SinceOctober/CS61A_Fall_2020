0b7097afafa24957f7e52d564c3097b403ba00ec
SinceOctober <281491295@qq.com>
2025-04-14 16:44:26 +0100

    Update

--- cats/cat.py
+++ cats/cat.py
@@ -66,7 +66,8 @@ def accuracy(typed, reference):
     # BEGIN PROBLEM 3
     if not typed_words:  # If nothing was typed
         return 0.0
-        
+    
+    
     correct = 0
     for i in range(min(len(typed_words), len(reference_words))):
         if typed_words[i] == reference_words[i]:
